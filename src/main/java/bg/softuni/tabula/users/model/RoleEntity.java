dae01d1141ea772a4fcd56452b5ad2ef1440b5d8
StilyanKirilov <60314753+StilyanKirilov@users.noreply.github.com>
2020-10-07 15:10:14 +0300

    Initial implementation of authentication.

---
+++ src/main/java/bg/softuni/tabula/users/model/RoleEntity.java
@@ -0,0 +1,19 @@
+package bg.softuni.tabula.users.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class RoleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private long id;
+
+    @Column(name = "role", nullable = false)
+    private String role;
+}
