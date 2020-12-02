e34a3045f0e2fe34ff92f3925bdcf923d5f05a74
StilyanKirilov <60314753+StilyanKirilov@users.noreply.github.com>
2020-12-02 09:27:31 +0200

    Adding Interceptor.

---
+++ src/main/java/bg/softuni/tabula/stats/StatsController.java
@@ -0,0 +1,24 @@
+package bg.softuni.tabula.stats;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StatsController {
+
+    private final StatsService statsService;
+
+    @Autowired
+    public StatsController(StatsService statsService) {
+        this.statsService = statsService;
+    }
+
+    @GetMapping("/stats")
+    public String stats(Model model) {
+        model.addAttribute("requestCount", statsService.getRequestCount());
+        model.addAttribute("startedOn", statsService.getStartedOn());
+        return "stats/stats";
+    }
+}
